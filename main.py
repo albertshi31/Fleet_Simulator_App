1c0e44e42d119e655f90745c423bdd86adbd79e9
Gabriel Laniewski <laniewskigabriel@gmail.com>
2021-11-01 06:57:02 -0400

    changed to absolute paths

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ global loop_length
 @app.route("/")
 @app.route("/setup")
 def setup():
-    html = render_template("setup.html")
+    html = render_template("/home/glaniewski/Fleet_Simulator_App/templates/setup.html")
     response = make_response(html)
     return response
 
@@ -29,12 +29,12 @@ def create_animation():
     fleetsize = int(request.args.get('fleetsize'))
     modesplit = int(request.args.get('modesplit'))
     aDispatcher = Dispatcher()
-    aDispatcher.createDataFeed("static/Trenton_AV_Station.csv", ["static/FinalOriginPixel34021_1.csv", "static/FinalOriginPixel34021_2.csv"], 40.194431, 40.259060, -74.808413, -74.720080, modesplit)
+    aDispatcher.createDataFeed("/home/glaniewski/Fleet_Simulator_App/static/Trenton_AV_Station.csv", ["/home/glaniewski/Fleet_Simulator_App/static/FinalOriginPixel34021_1.csv", "/home/glaniewski/Fleet_Simulator_App/static/FinalOriginPixel34021_2.csv"], 40.194431, 40.259060, -74.808413, -74.720080, modesplit)
     aDispatcher.createNumVehicles(fleetsize)
     trips, metrics, metric_animations, loop_length, run_time = aDispatcher.solve()
-    with open("static/depotbuildings.csv", "r") as f:
+    with open("/home/glaniewski/Fleet_Simulator_App/static/depotbuildings.csv", "r") as f:
         buildings = json.load(f)
-    html = render_template("create_animation.html", run_time = run_time)
+    html = render_template("/home/glaniewski/Fleet_Simulator_App/templates/create_animation.html", run_time = run_time)
     response = make_response(html)
     return response
 
@@ -45,7 +45,7 @@ def my_index():
     global metric_animations
     global buildings
     global loop_length
-    html = render_template("index.html", buildings=buildings, trips = json.loads(trips), metric_animations = json.loads(metric_animations), loop_length = loop_length)
+    html = render_template("/home/glaniewski/Fleet_Simulator_App/templates/index.html", buildings=buildings, trips = json.loads(trips), metric_animations = json.loads(metric_animations), loop_length = loop_length)
     response = make_response(html)
     return response
 
@@ -53,6 +53,6 @@ def my_index():
 @app.route("/graphs")
 def graph_page():
     global metrics
-    html = render_template("graphs.html", metrics = json.loads(metrics))
+    html = render_template("/home/glaniewski/Fleet_Simulator_App/templates/graphs.html", metrics = json.loads(metrics))
     response = make_response(html)
     return response
